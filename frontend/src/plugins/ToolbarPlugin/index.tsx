db71cebcebdf0c104be8f8a61141b6a13a54fb92
ju-ki <juki.programming@gmail.com>
2024-10-07 22:54:31 +0900

    #44 ツールバーを画面上部に固定化

--- frontend/src/plugins/ToolbarPlugin/index.tsx
+++ frontend/src/plugins/ToolbarPlugin/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { LinkToolbarItem } from '../LinkToolbarPlugin';
 import FontSizeItem from '../FontSizePlugin';
 import { TextColorItem } from '../TextColorPlugin';
@@ -9,8 +9,29 @@ import { HeadingItems } from '../HeadingPlugin';
 import QuoteItem from '../QuotePlugin';
 
 export const ToolbarPlugin: FC = () => {
+  const toolbarRef = useRef<HTMLDivElement>(null);
+  const [isFixed, setIsFixed] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const toolbar = document.querySelector('.toolbar');
+      if (toolbar) {
+        const distanceToTop = toolbar.getBoundingClientRect().top;
+        const scrollY = window.scrollY;
+
+        setIsFixed(scrollY > distanceToTop);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
-    <div className='flex items-center'>
+    <div
+      ref={toolbarRef}
+      className={`toolbar flex items-center transition-all duration-300 ${
+        isFixed ? 'fixed top-10 bg-white shadow-md z-10' : ''
+      }`}
+    >
       <HeadingItems />
       <QuoteItem />
       <LinkToolbarItem />
